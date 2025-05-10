1a4cee3cee580b1ea85226feca4707d5b115fe4f
Mikel12455 <105666282+Mikel12455@users.noreply.github.com>
2025-05-10 17:55:57 +0200

    feat: :sparkles: Aggiunto test ping a google.com
    Implementata feature per pingare google.com quando si visita downtime-panda

--- src/downtime_panda/__init__.py
+++ src/downtime_panda/__init__.py
@@ -1,11 +1,15 @@
+import requests
 from flask import Flask
 from flask.templating import render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index() -> str:
-    return render_template("index.html")
-    
+    r = requests.head("https://www.google.com")
+    return render_template("index.html", response=r)
+
+
 def main() -> None:
     app.run()
