fe7f5da5080395fc9472ac548d044ad91c0abd47
Mikel12455 <105666282+Mikel12455@users.noreply.github.com>
2025-05-10 11:54:13 +0200

    feat: :sparkles: Aggiunta rotta indice come prova

--- src/downtime_panda/__init__.py
+++ src/downtime_panda/__init__.py
@@ -1,3 +1,11 @@
-def main() -> None:
-    print("Hello from downtime-panda!")
+from flask import Flask
+from flask.templating import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index() -> str:
+    return render_template("index.html")
     
+def main() -> None:
+    app.run()
