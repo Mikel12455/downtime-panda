b2b2d11651f83376ac86fc3a09ea6e0f803bec39
Mikel12455 <105666282+Mikel12455@users.noreply.github.com>
2025-05-16 17:12:46 +0200

    fix: :card_file_box: Risolto problema sulla migrazione `crea_tabella_servizio`
    Cambiati gli apici da ` a "

--- database/migrations/V20250514193420__crea_tabella_servizio.sql
+++ database/migrations/V20250514193420__crea_tabella_servizio.sql
@@ -1,8 +1,8 @@
 -- Migration 2025/05/14 19:34:20
-CREATE SCHEMA IF NOT EXISTS `downtime_panda`;
+CREATE SCHEMA IF NOT EXISTS "downtime_panda";
 
-CREATE TABLE IF NOT EXISTS `downtime_panda`.`service` (
-        `id` BIGINT PRIMARY KEY,
-        `name` VARCHAR(64),
-        `uri` VARCHAR(255)
+CREATE TABLE IF NOT EXISTS "downtime_panda"."service" (
+        "id" BIGINT PRIMARY KEY,
+        "name" VARCHAR(64),
+        "uri" VARCHAR(255)
     );
