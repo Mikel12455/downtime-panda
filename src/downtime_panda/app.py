f058f352755ecdfacd7db3f7007feecafd528c73
Mikel12455 <105666282+Mikel12455@users.noreply.github.com>
2025-05-19 17:57:16 +0200

    feat: :sparkles: Prima implementazione creazione dei servizi
    Una prima implementazione della creazione di nuovi servizi sul database, assieme al loro ping

--- src/downtime_panda/app.py
+++ src/downtime_panda/app.py
@@ -1,16 +1,77 @@
+import base64
+import os
 import time
 
 import requests
-from flask import Flask, Response
+from flask import Flask, Response, redirect, request, url_for
 from flask.templating import render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 app = Flask(__name__)
+
+
+def b64encode(data: str):
+    return base64.b64encode(data.encode()).decode()
+
+
+app.jinja_env.filters["b64encode"] = b64encode
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+db = SQLAlchemy(model_class=Base)
+
+DB_DIALECT = os.environ.get("DTPANDA_DB_DIALECT")
+DB_HOST = os.environ.get("DTPANDA_DB_HOST")
+DB_PORT = os.environ.get("DTPANDA_DB_PORT")
+DB_USER = os.environ.get("DTPANDA_DB_USER")
+DB_PASSWORD = os.environ.get("DTPANDA_DB_PASSWORD")
+DB_DATABASE = os.environ.get("DTPANDA_DB_DATABASE")
+
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    f"{DB_DIALECT}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_DATABASE}"
+)
+db.init_app(app)
+
+
+class Service(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column()
+    uri: Mapped[str] = mapped_column()
+
+
+with app.app_context():
+    db.create_all()
 
 
 # ----------------------------------- HOME ----------------------------------- #
 @app.route("/")
 def index() -> str:
     return render_template("index.html.jinja")
+
+
+# ---------------------------------- SERVICE --------------------------------- #
+@app.route("/service/create", methods=["GET", "POST"])
+def service_create():
+    if request.method == "POST":
+        service = Service(
+            name=request.form["name"],
+            uri=request.form["uri"],
+        )
+        db.session.add(service)
+        db.session.commit()
+        return redirect(url_for("service_detail", id=service.id))
+
+    return render_template("service_create.html.jinja")
+
+
+@app.route("/service/<int:id>")
+def service_detail(id):
+    service = db.get_or_404(Service, id)
+    return render_template("service_detail.html.jinja", service=service)
 
 
 @app.route("/stream/ping/<website>")
@@ -21,6 +82,10 @@ def ping(website: str):
             yield f"data: {response.status_code}\n\n"
             time.sleep(5)
 
+    try:
+        website = base64.urlsafe_b64decode(website.encode()).decode()
+    except Exception:
+        return Response("Invalid base64 encoding", status=400)
     if not (website.startswith("http://") or website.startswith("https://")):
         website = f"https://{website}"
 
